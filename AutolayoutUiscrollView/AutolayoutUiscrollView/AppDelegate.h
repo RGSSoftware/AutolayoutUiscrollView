75a4e897f1462ab0e5120356bc4bc85367776ba9
Randel <homankids@gmail.com>
2016-05-14 08:40:23 -0500

    Added project.

---
+++ AutolayoutUiscrollView/AutolayoutUiscrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutUiscrollView
+//
+//  Created by PC on 5/14/16.
+//  Copyright © 2016 Randel Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
